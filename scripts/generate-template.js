7f2e2d53a7a6de37a24fdec8c8f5284842f3180d
abarisic1509 <abarisic@protonmail.com>
2024-08-22 15:12:21 +0200

    include predefined components setup

--- scripts/generate-template.js
+++ scripts/generate-template.js
@@ -34,6 +34,12 @@ const questions = [
     name: "useI18next",
     message: "Would you like to include i18next for internationalization?",
   },
+
+  {
+    type: "confirm",
+    name: "includeComponents",
+    message: "Would you like to include predefined components?",
+  },
 ];
 
 async function generateTemplate() {
@@ -42,7 +48,7 @@ async function generateTemplate() {
 
   let projectName =
     answers.projectName === "." ? getCurrentFolderName() : answers.projectName;
-  const { framework, language, useI18next } = answers;
+  const { framework, language, useI18next, includeComponents } = answers;
 
   // Determine the path of the selected template
   const templatePath = path.join(
@@ -50,6 +56,14 @@ async function generateTemplate() {
     framework.toLowerCase() === "react" ? "react" : "next",
     language.toLowerCase() === "javascript" ? "js" : "ts",
     useI18next ? "with-translation" : "base"
+  );
+
+  const componentsPath = path.join(
+    templatesDir,
+    `${framework.toLowerCase() === "react" ? "react" : "next"}/${
+      language.toLowerCase() === "javascript" ? "js" : "ts"
+    }`,
+    "components"
   );
 
   // Create the target directory if it doesn't exist
@@ -60,10 +74,22 @@ async function generateTemplate() {
     await fs.ensureDir(targetPath);
 
     await fs.copy(templatePath, targetPath);
+
+    // If components are included, copy the components folder separately
+    if (includeComponents) {
+      const targetComponentsPath = path.join(
+        targetPath,
+        framework.toLowerCase() === "react"
+          ? "src/ui/components"
+          : "ui/components"
+      );
+      await fs.ensureDir(path.dirname(targetComponentsPath)); // Ensure the directory exists
+      await fs.copy(componentsPath, targetComponentsPath);
+      console.log("Predefined components included.");
+    }
+
     console.log(
-      `Template ${framework} ${language} ${
-        useI18next ? "with i18next" : "without i18next"
-      } ${
+      `Template ${framework} ${language} ${useI18next ? "with i18next" : ""} ${
         includeComponents ? "and including predefined components" : ""
       } copied to ${projectName}`
     );
---
+++ templates/next/js/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button className="w-80 h-10 flex items-center justify-center bg-blue-500 text-white">
+      Button
+    </button>
+  );
+};
+
+export default Button;
